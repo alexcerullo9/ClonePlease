7c29f0a036f62bd7b9bdef1de1aaf9273a697525
alexcerullo9 <156937189+alexcerullo9@users.noreply.github.com>
2024-01-24 19:42:57 -0700

    fun

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
 def hello_world():
-	return "Hello" 
-	alex 
+	dummy = 'Hello World'
+	return dummy 
+def hello_world_n(N):
+	return " ".join(["Hello World!" for _ in range(N)])
+
+result_hello_world = hello_world()
+result_hello_world_n = hello_world_n(3)
+
+print(result_hello_world)
+print(result_hello_world_n)
+
